cda9dc7de00eba5419024a44165ab347d2e6c2bf
ZombieCatcher65 <48635501+ZombieCatcher65@users.noreply.github.com>
2025-03-05 17:23:03 +1100

    Add files via upload

---
+++ language.py
@@ -0,0 +1,18 @@
+from slyguy.language import BaseLanguage
+
+
+class Language(BaseLanguage):
+    LAT_LONG              = 30001
+    HIDE_CLIPS            = 30002
+    NEWS                  = 30008
+    CATEGORIES            = 30009
+    FEATURED              = 30012
+    SHOWS_LETTER          = 30013
+    HIDE_SUGGESTED        = 30014
+    FLATTEN_SINGLE_SEASON = 30015
+    SUGGESTED             = 30016
+    NO_VIDEO              = 30018
+    SEASON                = 30019
+
+
+_ = Language()
